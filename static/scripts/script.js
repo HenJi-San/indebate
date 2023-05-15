8201e15ad4a0ccf143238a78acc7b0ea040a5bf0
WhyNot <henji0905@gmail.com>
2023-05-15 20:41:59 +0300

    15.05.23 (2)

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,51 +1,31 @@
-function forward() {
-    anime({
-      targets: '.menu-small',
-      translateX: ['-100%', '0'],
-      easing: 'easeInOutQuad',
-      direction: 'alternate',
-      duration: 500,
-      loop: false,
-    });
-    condition = false;
-  }
-  
-  function backward() {
-    anime({
-      targets: '.menu-small',
-      translateX: ['0', '-100%'],
-      easing: 'easeInOutQuad',
-      direction: 'alternate',
-      duration: 500,
-      loop: false,
-    });
-    condition = true;
-  }
-  
-  let condition = true;
-  $('.menu_small_icon').click(function () {
-    if (condition) {
-      forward();
-    } else {
-      backward();
+let indebateText = document.querySelectorAll("._animScroll")
+
+if (indebateText.length > 0) {
+  window.addEventListener("scroll", indebateOnScroll);
+  function indebateOnScroll(params) {
+    for (let index = 0; index < indebateText.length; index++) {
+      const indebateTextConst = indebateText[index];
+      const indebateTextHeight = indebateTextConst.offsetHeight;
+      const indebateTextOffset = offset(indebateTextConst).top;
+      const indebateTextStart = 4000;
+
+      let indebateTextPoint = window.innerHeight - indebateTextHeight / indebateTextStart;
+
+      if (indebateTextHeight > window.innerHeight) {
+        indebateTextPoint = window.innerHeight - window.innerHeight / indebateTextStart;
+      }
+      if ((pageYOffset > indebateTextOffset - indebateTextPoint) && pageYOffset < (indebateTextOffset + indebateTextHeight)) {
+        indebateTextConst.classList.add('_active');
+      } else {
+        indebateTextConst.classList.remove('_active');
+      }
     }
-  });
-  
-  let start = 0;
-  
-  let end = 0;
-  
-  $('.contaiter-fuild').on('touchstart', function (event) {
-    start = event.originalEvent.touches[0].pageX;
-  });
-  
-  $('.contaiter-fuild').on('touchend', function (event) {
-    end = event.originalEvent.changedTouches[0].pageX;
-  
-    if (end - start >=100 && condition) {
-      forward();
-    } else if (start - end >=100 && !condition) {
-      backward();
-    }
-  });
-  
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+  indebateOnScroll();
+}
